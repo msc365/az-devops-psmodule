e2a24bf5ef046ec2b6e76d01b15a89f8c321bc58
Martin Swinkels <120459644+MSc365Admin@users.noreply.github.com>
2025-12-31 12:09:05 +0000

    chore: Update github issue templates (#21)
    - chore: Update issue templates to improve user input fields and descriptions
    - fix: Update pre-release label format in build script

--- src/Build.ps1
+++ src/Build.ps1
@@ -29,7 +29,7 @@ Properties {
     $script:buildVersion = [System.Version]'0.2.0'
 
     # Pre-release label (e.g. 'alpha1', 'beta1', 'rc1'). Set to $null for stable releases.
-    $script:prerelease = 'alpha.1'
+    $script:prerelease = 'alpha1'
 
     # The root path of the repository.
     $script:rootPath = (Get-Item $PSScriptRoot).Parent.FullName
